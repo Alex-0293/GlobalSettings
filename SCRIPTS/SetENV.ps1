bb73af14ab6c47409d26cef7003876b49a69b62e
Alex-0293 <1928311@tuta.io>
2020-05-27 19:12:06 +0300

    Add SetENV.ps1.

---
+++ SCRIPTS/SetENV.ps1
@@ -0,0 +1,2 @@
+[Environment]::SetEnvironmentVariable( "AlexKFrameworkInitScript", "C:\Users\Alex\Documents\PROJECTS\GlobalSettings\SCRIPTS\Init.ps1", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable( "AlexKFrameworkGlobalInitScript", "C:\Users\Alex\Documents\PROJECTS\GlobalSettings\SCRIPTS\InitGlobal.ps1", [EnvironmentVariableTarget]::Machine)
